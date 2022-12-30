03b6ecea4a2e89a29e70b6936beab8c477013cbf
Muhammet Ali Çamlıca <Mecatr60@gmail.com>
2022-12-30 10:30:31 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace gectkaldi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+       Console.WriteLine("FİYAT GİRİNİZ..");
+        var girilenfiyat = Console.ReadLine();
+
+        var sonuc = (girilenfiyat  (18/ 100));
+
+        Console.WriteLine("KDVLİ FİYAT : " + sonuc);
+
+    }
+
+}
